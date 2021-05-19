39df9277ebc99305ff60a022695899de9bf203cf
Carlos Vinícius Souza <66135817+Viniiixz@users.noreply.github.com>
2021-05-18 21:45:13 -0300

    Update HomeController.cs

--- softWeb/Controllers/HomeController.cs
+++ softWeb/Controllers/HomeController.cs
@@ -15,7 +15,51 @@ namespace softWeb.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        
+        ConnectSQLServer con = new ConnectSQLServer();
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+
+        List<getCustomers> listCountCustomer = new List<getCustomers>();
+
+        string queryCountCustomer = "select count(CustomerKey) from DimCustomer";
+
+        public void getCountCustomer()
+        {
+            if (listCountCustomer.Count > 0)
+            {
+                listCountCustomer.Clear();
+            }
+            try
+            {
+                con.Connect();
+                com.Connection = con.Connect();
+                com.CommandText = queryCountCustomer;
+                dr = com.ExecuteReader();
+                while (dr.Read())
+                {
+                    listCountCustomer.Add(new getCustomers()
+                    {
+                        CustomerKey = dr["CustomerKey"].ToString(),
+                        FirstName = dr["FirstName"].ToString(),
+                        MiddleName = dr["MiddleName"].ToString(),
+                        LastName = dr["LastName"].ToString(),
+                        BirthDate = dr["BirthDate"].ToString(),
+                        MaritalStatus = dr["MaritalStatus"].ToString(),
+                        Gender = dr["Gender"].ToString(),
+                        EmailAddress = dr["EmailAddress"].ToString(),
+                        EnglishOccupation = dr["EnglishOccupation"].ToString(),
+                        AddressLine1 = dr["AddressLine1"].ToString(),
+                        Phone = dr["Phone"].ToString()
+                    });
+                }
+                con.Disconnect();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
@@ -23,7 +67,8 @@ namespace softWeb.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            getCountCustomer();
+            return View(listCountCustomer);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
